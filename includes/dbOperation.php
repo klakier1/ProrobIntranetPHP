e871f62d00ccecde05226151c09ed903db9bc984
Lukasz Paleczel <lukasz.paleczek@gmail.com>
2019-06-11 16:24:51 +0200

    Create user 1th test

--- includes/dbOperation.php
+++ includes/dbOperation.php
@@ -159,7 +159,7 @@
 
             if($query->execute()){
                 if($query->rowCount() == 1){
-                    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+                    $result = $query->fetch(PDO::FETCH_ASSOC);
                     return GET_USERS_SUCCESS;
                 }else{
                     return GET_USERS_NOT_FOUND;
