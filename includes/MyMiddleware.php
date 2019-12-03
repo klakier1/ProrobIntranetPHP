35387d8e856a96834a37632606da403379411f81
Lukasz Paleczek <lukasz.paleczek@gmail.com>
2019-12-03 15:01:43 +0100

    Beta 0.1

--- includes/MyMiddleware.php
+++ includes/MyMiddleware.php
@@ -8,27 +8,37 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 final class MyMiddleware
 {
+    private $allowedPath = "/";
+
+    public function __construct(array $options = [])
+    {
+        if (isset($options["path"])) {
+            $allowedPath = $options["path"];
+        }
+    }
+
     public function __invoke(
         ServerRequestInterface $request,
         ResponseInterface $response,
         callable $next
     ): ResponseInterface {
-        $token = $request->getAttribute("decoded_token_data");
-        $role = checkTokenData($token);
-        if($role == TOKEN_ADMIN || $role == TOKEN_EMPLOYEE)
-        {
-            $request = $request->withAttribute("role", $role);  //set Attribute
-            $response = $next($request, $response);             //call next middleware
-            return $response;
+
+        $path = "/" . $request->getUri()->getPath();
+        if (strpos($path, $this->allowedPath) === 0) {
+
+            $token = $request->getAttribute("decoded_token_data");
+            $role = checkTokenData($token);
+            if ($role == TOKEN_ADMIN || $role == TOKEN_EMPLOYEE) {
+                $request = $request->withAttribute("role", $role);  //set Attribute
+                $response = $next($request, $response);             //call next middleware
+                return $response;
+            } else {
+                $response = standardResponse($response, 401, true, "Token verification failed");
+                return $response;
+            }
+        } else { 
+            $response = $next($request, $response); 
+            return $response; 
         }
-        else
-        {
-            $response = standardResponse($response, 401, true, "Token verification failed");
-            return $response;
-        }
-       
     }
-
 }
-
-?>
