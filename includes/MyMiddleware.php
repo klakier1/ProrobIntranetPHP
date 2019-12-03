7cc5b5cebb09444859ec2afa93305ba8c9c82ded
Lukasz Paleczek <lukasz.paleczek@gmail.com>
2019-12-03 21:43:25 +0100

    Beta 0.2

--- includes/MyMiddleware.php
+++ includes/MyMiddleware.php
@@ -23,36 +23,25 @@ final class MyMiddleware
         callable $next
     ): ResponseInterface {
 
-        echo $request->getUri()->getPath()[0];
-        echo "\n";
-        echo $request->getUri()->getPath();
-        echo "\n";
-
         $path = $request->getUri()->getPath();
 
+        //Some servers return path as "api/something" but other "/api/something"
         if ($request->getUri()->getPath()[0] != "/")
             $path = "/" . $path;
 
-        echo $path;
-
         if (strpos($path, $this->allowedPath) === 0) {
-
             $token = $request->getAttribute("decoded_token_data");
             $role = checkTokenData($token);
             if ($role == TOKEN_ADMIN || $role == TOKEN_EMPLOYEE) {
                 $request = $request->withAttribute("role", $role);  //set Attribute
-                echo "DIRECTLY_BEFORE OK";
                 $response = $next($request, $response);             //call next middleware
-                echo "AFTER_OK";
                 return $response;
             } else {
                 $response = standardResponse($response, 401, true, "Token verification failed");
                 return $response;
             }
         } else {
-            echo "BEFORE_NOK";
             $response = $next($request, $response);
-            echo "AFTER_NOK";
             return $response;
         }
     }
