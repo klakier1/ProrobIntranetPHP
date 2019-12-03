32c4dcfce99eb4137fa5f93621c8fce7fbf8ec63
Lukasz Paleczek <lukasz.paleczek@gmail.com>
2019-12-03 21:36:15 +0100

    Beta 0.2

--- includes/MyMiddleware.php
+++ includes/MyMiddleware.php
@@ -31,21 +31,27 @@ final class MyMiddleware
         if ($request->getUri()->getPath()[0] != "/") {
             $path = "/" . $request->getUri()->getPath();
         }
-        echo $path;
-        if (strpos($path, $this->allowedPath) === 0) {
 
+        echo $path;
+
+        if (strpos($path, $this->allowedPath) === 0) {
+            echo "BEFORE_OK";
             $token = $request->getAttribute("decoded_token_data");
             $role = checkTokenData($token);
             if ($role == TOKEN_ADMIN || $role == TOKEN_EMPLOYEE) {
                 $request = $request->withAttribute("role", $role);  //set Attribute
+                echo "DIRECTLY_BEFORE OK";
                 $response = $next($request, $response);             //call next middleware
+                echo "AFTER_OK";
                 return $response;
             } else {
                 $response = standardResponse($response, 401, true, "Token verification failed");
                 return $response;
             }
         } else {
+            echo "BEFORE_NOK";
             $response = $next($request, $response);
+            echo "AFTER_NOK";
             return $response;
         }
     }
