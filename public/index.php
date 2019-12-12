efd82ed324f8529565129a33308629097a71b8d0
Lukasz Paleczek <lukasz.paleczek@gmail.com>
2019-12-12 21:03:25 +0100

    beta 0.4 HEROKU DB

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ require '../includes/responseProcess.php';
 require '../includes/dbOperation.php';
 require '../includes/token.php';
 
-use \Klakier\ErrorHandlerProvider;
+use Klakier\ErrorHandlerProvider;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
